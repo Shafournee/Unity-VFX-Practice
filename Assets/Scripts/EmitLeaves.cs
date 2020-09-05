e8cc11f80c28068a38beac178a1323de4416c0ae
Shane Lesser <shafournee@gmail.com>
2020-09-04 22:17:27 -0400

    Added leaf particle effect

---
+++ Assets/Scripts/EmitLeaves.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitLeaves : MonoBehaviour
+{
+    [SerializeField] ParticleSystem leafParticles = default;
+    [SerializeField] int minParticleCount = default;
+    [SerializeField] int maxParticleCount = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<PlayerController>(out var PlayerController))
+        {
+            Rigidbody2D playerRigidbody = collision.GetComponent<Rigidbody2D>();
+            var emitParams = new ParticleSystem.EmitParams();
+            emitParams.velocity = playerRigidbody.velocity / 7f;
+
+            int particleCount = Random.Range(minParticleCount, maxParticleCount);
+
+            leafParticles.Emit(emitParams, particleCount);
+        }
+    }
+}
