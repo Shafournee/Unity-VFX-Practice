78d3b84c7803665e1175f87ab016071ac6215a6d
Shane Lesser <shafournee@gmail.com>
2020-09-08 19:59:24 -0400

    Finished up the particle systems

---
+++ Assets/Scripts/EmitApples.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitApples : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<PlayerController>(out var PlayerController))
+        {
+            GetComponent<ParticleSystem>().Emit(5);
+        }
+    }
+}
--- Assets/Scripts/EmitLeaves.cs
+++ Assets/Scripts/EmitLeaves.cs
@@ -8,18 +8,6 @@ public class EmitLeaves : MonoBehaviour
     [SerializeField] int minParticleCount = default;
     [SerializeField] int maxParticleCount = default;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<PlayerController>(out var PlayerController))
